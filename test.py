95559dda97ca1518b2aec890da4fc6dc91c70b32
Steve Davidson <buzzdavidson@me.com>
2011-04-27 20:25:27 -0700

    Minor changes for linux and display

--- test.py
+++ test.py
@@ -3,27 +3,27 @@ import openzwave
 options = openzwave.PyOptions()
 
 # Specify the open-zwave config path here
-options.create("c:/workspace/py-openzwave/openzwave/config/","","")
+options.create("py-openzwave/openzwave/config/","","")
 options.lock()
 manager = openzwave.PyManager()
 manager.create()
 
+cbstrs=['value added','value removed','value changed','groups changed','new node','node added',
+'node removed','node protocol info','node naming','node event','polling disabled',
+'polling enabled','driver ready','driver reset','message complete','node queries complete',
+'awake nodes queried','all nodes queried']
+
 def callback(type, *args):
-    
-    if type == 5:
-        # Test callback of Type_NodeAdded, called after a node has been added to open-zwave.
-        homeid = args[0]
-        nodeid = args[1]
-        
-    elif type == 0:
-        print "value added"
-        print "type is:", type
-        print "args is:", args
-        
-    elif type == 2:
-        print "value changed"
-        print "type is:", type
-        print "args is:", args
+    print('\n%s\n[%s]: type=%d\n' % ('-'*20, cbstrs[type], type))
+    if args:
+        print('homeId: %x' % args[0])
+        print('nodeId: %d' % args[1])
+        i = 2
+        for arg in args[2:]:
+            print('arg[%d]: %d (0x%.2x)' % (i, arg, arg))
+            i += 1
+    print('%s\n' % ('-'*20,))
 
 manager.add_watcher(callback)
-manager.add_driver("\\\\.\\COM16")
+manager.add_driver('/dev/ttyUSB6')
+ 
