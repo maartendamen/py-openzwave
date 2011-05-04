fb804b0a649692678b69a5e4f2332a0078ff3951
Steve Davidson <buzzdavidson@me.com>
2011-05-04 00:11:14 -0700

    Update to support DrewP's new callback implementation

--- test.py
+++ test.py
@@ -4,24 +4,26 @@ from openzwave import PyManager
 options = openzwave.PyOptions()
 
 # Specify the open-zwave config path here
-options.create("/home/steve/py-openzwave/openzwave/config/","","")
+options.create("openzwave/config/","","")
 options.lock()
 manager = openzwave.PyManager()
 manager.create()
 
 # callback order: (notificationtype, homeid, nodeid, ValueID, groupidx, event)
-def callback(type, *args):
-    print('\n%s\n[%s]:\n' % ('-'*20, PyManager.CALLBACK_DESC[type]))
+def callback(args):
+    print('\n%s\n[%s]:\n' % ('-'*20, args['notificationType']))
     if args:
-        print('homeId: 0x%.8x' % args[0])
-        print('nodeId: %d' % args[1])
-        print('valueID: %s' % args[2])
-        if (args[3] != 0xff): print('GroupIndex: %d' % args[3])
-        if (args[4] != 0xff): print('Event: %d' % args[4])
-        if (args[5]): print('Value: %s' % str(args[5]))
-        if (args[6]): print('Label: %s' % args[6])
-        if (args[7]): print('Units: %s' % args[7])
+        print('homeId: 0x%.8x' % args['homeId'])
+        print('nodeId: %d' % args['nodeId'])
+        v = args['valueId']
+        print('valueID: %s' % v['id'])
+        if v.has_key('groupIndex') and v['groupIndex'] != 0xff: print('GroupIndex: %d' % v['groupIndex'])
+        if v.has_key('event') and v['event'] != 0xff: print('Event: %d' % v['event'])
+        if v.has_key('value'): print('Value: %s' % str(v['value']))
+        if v.has_key('label'): print('Label: %s' % v['label'])
+        if v.has_key('units'): print('Units: %s' % v['units'])
+        if v.has_key('readOnly'): print('ReadOnly: %s' % v['readOnly'])
     print('%s\n' % ('-'*20,))
 
 manager.addWatcher(callback)
-manager.addDriver('/dev/keyspan-1')
+manager.addDriver('/dev/keyspan-2')
