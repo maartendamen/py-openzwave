07cd639e6932cc17cd4ee5e0ce986dae00e2b078
Steve Davidson <buzzdavidson@me.com>
2011-05-05 03:52:18 -0700

    Add progress dialog and simple dialog box primitives, first pass display item nodes

--- ozwCurses.py
+++ ozwCurses.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from collections import namedtuple
 
 import curses
 import curses.panel
@@ -9,9 +10,12 @@ import time
 from louie import dispatcher, All
 from ozwWrapper import ZWaveWrapper
 
+padcoords = namedtuple('padcoords', ['sminrow','smincol','smaxrow','smaxcol'])
+
 class ZWaveCommander:
     def __init__(self, stdscr):
-        print('init')
+        self._curAlert = False
+        self._alertStack = list()
         self._driverInitialized = False
         self._wrapper = None
         self._listMode = True
@@ -36,6 +40,7 @@ class ZWaveCommander:
 
         self._config = {
             'device': '/dev/keyspan-2',
+            'config': 'openzwave/config/',
         }
 
         # TODO: add log level to config
@@ -61,10 +66,15 @@ class ZWaveCommander:
         self._log.debug('thread %s sleeping...', context)
         time.sleep(duration)
         self._log.debug('timer %s expired, executing callback %s', context, callback)
+        if context == 'alert':
+            self._curAlert = False
+            if self._alertStack:
+                self._alert(self._alertStack.pop())
         if callback is not None:
             callback()
 
     def _handleQuit(self):
+        # TODO: exit confirmation dialog
         self._log.info('Stop requested')
         self._stop.set()
         
@@ -75,11 +85,15 @@ class ZWaveCommander:
 
     def _alert(self, text):
         '''perform program alert'''
-        curses.flash()
-        self._screen.addstr(self._screensize[0] - 1, 0, ' {0:{width}}'.format(text, width=self._screensize[1] - 2),
+        if not self._curAlert:
+            self._curAlert = True
+            curses.flash()
+            self._screen.addstr(self._screensize[0] - 1, 0, ' {0:{width}}'.format(text, width=self._screensize[1] - 2),
                             curses.color_pair(self.COLOR_ERROR))
-        self._screen.refresh()
-        self._setTimer('alert', 2, self._redrawMenu)
+            self._screen.refresh()
+            self._setTimer('alert', 1, self._redrawMenu)
+        else:
+            self._alertStack.append(text)
 
     def _layoutScreen(self):
         # TODO: handle screen resize on curses.KEY_RESIZE in loop (tear down, re-calculate, and re-build)
@@ -141,17 +155,11 @@ class ZWaveCommander:
         self._log.debug('Adjusted row heights are: %s' , self._rowheights)
 
         self._listpad = curses.newpad(256,256)
-#        self._updateSystemInfo()
         self._updateColumnHeaders()
-#        self._updateDeviceList()
-#        self._updateDeviceDetail()
-#        self._updateMenu()
 
     def _initCurses(self, stdscr):
         '''Configure ncurses application-specific environment (ncurses has already been initialized)'''
         curses.curs_set(0)
-
-        # TODO: handle column sorting in list view
 
         # Re-define color attributes...
         self.COLOR_NORMAL=1
@@ -162,7 +170,6 @@ class ZWaveCommander:
         self.COLOR_WARN=6
         self.COLOR_OK=7
 
-        # set attron before write, attroff after to use other attributes
         curses.init_pair(self.COLOR_NORMAL, curses.COLOR_WHITE, curses.COLOR_BLACK) # normal (selected row is inverted, disabled/sleep is dim)
         curses.init_pair(self.COLOR_HEADER_NORMAL, curses.COLOR_BLACK, curses.COLOR_GREEN) # header normal
         curses.init_pair(self.COLOR_HEADER_HI, curses.COLOR_WHITE, curses.COLOR_CYAN) # header hi
@@ -172,23 +179,6 @@ class ZWaveCommander:
         curses.init_pair(self.COLOR_OK, curses.COLOR_GREEN, curses.COLOR_BLACK) # ok
 
         self._layoutScreen()
-
-        # curses.init_pair: init color pair
-        # curses.initscr: returns WindowObject with window dimensions (how does this relate to wrapper?)
-        # curses.newpad: like viewport
-        # curses.newwin: new window object; by default is whole screen
-
-        # window.addch/addstr/addnstr: write text
-        # window.attron/attroff/attrset: appearance control
-        # window.bkgnd: appearance control (background)
-        # window.border: lines, explicit control
-        # window.box: lines, default char
-        # window.chgat: change line attributes
-        # window.clear/clrtobot/clrtoeol/erase
-        # window.derwin: derive new sub-widow
-        # getch/getkey: read key as int/char
-        # window.getmaxyx: get window dimensions
-        # window.getstr: read string
 
     def _checkConfig(self):
         # TODO: check if configuration exists and is valid.  If not, then go directly to handleSetup().  Loop until user cancels or enters valid config.
@@ -207,34 +197,119 @@ class ZWaveCommander:
             self._log.info('OpenZWave initialized successfully.')
 
     def _notifyDriverReady(self, homeId):
-        # TODO: use correct accessor
-        self._log.info('OpenZWave Driver is Ready; homeid is %0.8x.  %d nodes were found.', homeId, len(self._wrapper._nodes))
+        self._log.info('OpenZWave Driver is Ready; homeid is %0.8x.  %d nodes were found.', homeId, self._wrapper.nodeCount)
         self._driverInitialized = True
+        self._addDialogText(2,'Driver initialized with homeid {0}'.format(hex(homeId)))
+        self._addDialogText(3,'Node Count is now {0}'.format(self._wrapper.nodeCount))
+        self._readyNodeCount = 0
+
+    def _notifyNodeAdded(self, homeId, nodeId):
+        self._addDialogText(3,'Node Count is now {0}'.format(self._wrapper.nodeCount))
         self._updateSystemInfo()
+
+    def _redrawAll(self):
+        # TODO: re-build and draw entire screen
+        self._clearDialog()
+        self._updateSystemInfo()
+        self._updateDeviceList()
 
     def _notifySystemReady(self):
         self._log.info('OpenZWave Initialization Complete.')
         self._alert('OpenZWave Initialization Complete.')
+        self._redrawAll()
 
-    def _notifyNodeReady(self, nodeId):
-        self._log.info('Node %d is ready.', nodeId)
-        self._alert('Node %d is ready.' % nodeId)
+    def _notifyNodeReady(self, homeId, nodeId):
+        self._readyNodeCount += 1
+        self._addDialogText(2, 'OpenZWave is querying associated devices')
+        self._addDialogText(3,'Node {0} is now ready'.format(nodeId))
+        self._addDialogProgress(5, self._readyNodeCount, self._wrapper.nodeCount)
+        #self._updateDeviceList() node's not fully populated here
 
     def _notifyValueChanged(self, signal, **kw):
         pass
+        # TODO: handle value changed notification
+
+    def _initDialog(self, height, width, buttons=('OK',), caption=None):
+        self._dialogpad = curses.newpad(height, width)
+        self._dialogpad.bkgd(0x94, curses.color_pair(self.COLOR_HEADER_HI))
+        self._dialogpad.clear()
+        self._dialogpad.box()
+        if caption:
+           lh = (width / 2) - (len(caption) / 2) - 1
+           self._dialogpad.addstr(0, lh, ' {0} '.format(caption), curses.color_pair(self.COLOR_NORMAL) | curses.A_STANDOUT)
+        if buttons:
+            if len(buttons) > 1:
+                bwid = 0
+                for bcap in buttons:
+                    if len(bcap) > bwid: bwid = len(bcap)
+                cellwid = (width - 4) / len(buttons)
+                lpad = (cellwid - bwid) / 2 - 1
+                rpad = cellwid - bwid - lpad - 1
+                self._dialogpad.move(height - 2, 1)
+            else:
+                bwid = len(buttons[0])
+                lpad = rpad = 1
+                self._dialogpad.move(height - 2, (width / 2) - (bwid / 2) - 2)
+            for button in buttons:
+                self._dialogpad.addstr('{0:{wlpad}}<{1:^{wbwid}}>{0:{wrpad}}'.format('',button, wlpad=lpad, wbwid=bwid, wrpad=rpad))
+        dt = (self._screensize[0] / 2) - (height / 2)
+        dl = (self._screensize[1] / 2) - (width / 2)
+        dc = padcoords(sminrow=dt,smincol=dl,smaxrow=dt+height - 1, smaxcol=dl+width - 1)
+        self._dialogcoords = dc
+        self._dialogpad.overlay(self._screen, 0, 0, dc.sminrow, dc.smincol, dc.smaxrow, dc.smaxcol)
+        self._screen.refresh()
+
+    def _clearDialog(self):
+        del self._dialogpad
+        self._dialogpad = None
+        self._dialogcoords = None
+        self._screen.touchwin()
+        self._screen.refresh()
+
+    def _updateDialog(self):
+        self._screen.refresh()
+        dc = self._dialogcoords
+        self._dialogpad.refresh(0,0,dc.sminrow, dc.smincol, dc.smaxrow, dc.smaxcol)
+
+    def _addDialogText(self, row, text, align='^'):
+        self._dialogpad.addstr(row, 1, '{0:{aln}{wid}}'.format(text, aln=align, wid=self._dialogpad.getmaxyx()[1] - 2))
+        self._updateDialog()
+
+    def _addDialogProgress(self, row, current, total, showPercent=True, width=None):
+        dc = self._dialogcoords
+        if width is None:
+            width = (dc.smaxcol - dc.smincol) * 2 / 3
+        pct = float(current) / float(total)
+        filled = int(pct * float(width))
+        lh = ((dc.smaxcol - dc.smincol) / 2) - (width / 2)
+        self._dialogpad.addch(row, lh - 1, '[', curses.color_pair(self.COLOR_NORMAL) | curses.A_BOLD)
+        self._dialogpad.addch(row, lh + width, ']', curses.color_pair(self.COLOR_NORMAL) | curses.A_BOLD)
+        self._dialogpad.addstr(row, lh, ' '*width, curses.color_pair(self.COLOR_NORMAL))
+        self._dialogpad.addstr(row, lh, '|'*filled, curses.color_pair(self.COLOR_OK) | curses.A_BOLD)
+        if showPercent:
+            pctstr = '{0:4.2%}'.format(pct)
+            lh = ((dc.smaxcol - dc.smincol) / 2) - (len(pctstr) / 2)
+            self._dialogpad.addstr(row, lh, pctstr, curses.color_pair(self.COLOR_NORMAL) | curses.A_BOLD)
+        self._updateDialog()
 
     def _checkInterface(self):
-        # TODO: exception handling?
         dispatcher.connect(self._notifyDriverReady, ZWaveWrapper.SIGNAL_DRIVER_READY)
         dispatcher.connect(self._notifySystemReady, ZWaveWrapper.SIGNAL_SYSTEM_READY)
         dispatcher.connect(self._notifyNodeReady, ZWaveWrapper.SIGNAL_NODE_READY)
         dispatcher.connect(self._notifyValueChanged, ZWaveWrapper.SIGNAL_VALUE_CHANGED)
+        dispatcher.connect(self._notifyNodeAdded, ZWaveWrapper.SIGNAL_NODE_ADDED)
+        self._initDialog(10,60,['Cancel'],'Progress')
+        self._addDialogText(2,'Initializing OpenZWave')
         self._log.info('Initializing OpenZWave via wrapper')
-        self._wrapper = ZWaveWrapper(device=self._config['device'], config='openzwave/config/', log=None)
-        self._setTimer('initCheck', 5, self._checkIfInitialized())
+        self._wrapper = ZWaveWrapper(device=self._config['device'], config=self._config['config'], log=None)
+        self._setTimer('initCheck', 3, self._checkIfInitialized)
+
+        while not self._stop.isSet() and not self._wrapper.initialized:
+            time.sleep(0.1)
+            # TODO: handle keys here...
 
     def _runLoop(self):
-        while not self._stop.isSet():
+        while not self._stop.isSet():   
             key = self._screen.getch()
             if key == curses.KEY_DOWN: self._switchItem(1)
             elif key == curses.KEY_UP: self._switchItem(-1)
@@ -294,11 +369,12 @@ class ZWaveCommander:
         self._screen.addstr(row, self._screensize[1] - len(data), data, attrs)
 
     def _updateSystemInfo(self):
-        self._screen.addstr(0,1,'HomeSeer Z-Troller on /dev/keyspan2', curses.color_pair(self.COLOR_NORMAL))
-        self._screen.addstr(1,1,'Home ID 0x003d8522', curses.color_pair(self.COLOR_NORMAL))
-        self._screen.addstr(2,1,'7 Registered Nodes (2 Sleeping) ', curses.color_pair(self.COLOR_NORMAL))
-        self._rightPrint(0, 'Installer Library')
-        self._rightPrint(1, 'Version Z-Wave 2.78')
+        self._screen.addstr(0,1,'{0} on {1}'.format(self._wrapper.controllerDescription, self._config['device']), curses.color_pair(self.COLOR_NORMAL))
+        self._screen.addstr(1,1,'Home ID 0x%0.8x' % self._wrapper.homeId, curses.color_pair(self.COLOR_NORMAL))
+        self._screen.addstr(2,1,'{0} Registered Nodes ({1} Sleeping)'.format(self._wrapper.nodeCount, self._wrapper.sleepingNodeCount), curses.color_pair(self.COLOR_NORMAL))
+        self._rightPrint(0, '{0} Library'.format(self._wrapper.libraryTypeName))
+        self._rightPrint(1, 'Version {0}'.format(self._wrapper.libraryVersion))
+        self._screen.refresh()
 
     def _updateColumnHeaders(self):
         self._screen.move(4,0)
@@ -320,12 +396,19 @@ class ZWaveCommander:
             self._screen.addstr(' {0:<{width}} '.format(text, width=wid), clr)
 
     def _updateDeviceList(self):
-        # TODO: sort based upon values
-        self._listcount = 128
+        # TODO: handle column sorting in list view
+        self._listcount = self._wrapper.nodeCount
         self._listpad.clear()
-        for i in range(0,128):
-            clr = curses.color_pair(self.COLOR_NORMAL) | curses.A_STANDOUT if i == self._listitem else curses.color_pair(self.COLOR_NORMAL)
-            self._listpad.addstr(i,0,' {0} {1:<{width}}'.format(i, 'This is a node', width=self._screensize[1]), clr)
+        idx = 0
+        for node in self._wrapper._nodes.itervalues():
+            #clr = curses.color_pair(self.COLOR_NORMAL) | curses.A_STANDOUT if i == self._listitem else curses.color_pair(self.COLOR_NORMAL)
+            # self._colheaders=['','ID','Name','Location','Type','State','Batt','Signal']
+            # TODO: add remaining columns
+            # TODO: add highlight
+            # TODO: for each column, ensure contents fit within width constraints
+            self._listpad.addstr(idx,0,' {0:<{w1}}{1:<{w2}}{2:<{w3}}{3:<{w4}}'.format(node.id, node.name, node.location, node.type, w1=self._colwidths[1], w2=self._colwidths[2], w3=self._colwidths[3], w4=self._colwidths[4]))
+            idx += 1
+
         ctop = self._rowheights[0]
         listheight = self._rowheights[1]
         if self._listitem - self._listtop > listheight:
