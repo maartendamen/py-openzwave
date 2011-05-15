b5b1aa2e16bcc76d61325ed4cc3320b2c09b02e4
Maarten Damen <m.damen@gmail.com>
2011-05-15 14:05:25 +0200

    Fixed setup.py to work on both Windows and Linux

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
+from os import name as os_name
 from distutils.core import setup
 from Cython.Distutils import extension
 from Cython.Distutils import build_ext
 
-ext_modules = [extension.Extension("openzwave", ["openzwave.pyx"],
-                         libraries=['udev', 'stdc++'],
-                         language="c++",
-                         extra_objects=['openzwave/cpp/lib/linux/openzwave.a'], 
-                         include_dirs=['openzwave/cpp/src', 'openzwave/cpp/src/value_classes', 'openzwave/cpp/src/platform']
-)]
+if os_name == 'nt':
+    ext_modules = [extension.Extension("openzwave", ["openzwave.pyx"],
+                             libraries=['setupapi', 'stdc++'],
+                             language="c++",
+                             extra_objects=['openzwave/cpp/lib/windows-mingw32/openzwave.a'],
+                             include_dirs=['openzwave/cpp/src', 'openzwave/cpp/src/value_classes', 'openzwave/cpp/src/platform']
+    )]    
+else:
+    ext_modules = [extension.Extension("openzwave", ["openzwave.pyx"],
+                             libraries=['udev', 'stdc++'],
+                             language="c++",
+                             extra_objects=['openzwave/cpp/lib/linux/openzwave.a'], 
+                             include_dirs=['openzwave/cpp/src', 'openzwave/cpp/src/value_classes', 'openzwave/cpp/src/platform']
+    )]
+
 setup(
   name = 'py-openzwave',
   author='Maarten Damen',
