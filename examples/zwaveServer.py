c4bc26ae3c6aa18da2d2d8762db452657591932e
Brian O'Dell <briantodell@gmail.com>
2011-08-04 12:05:53 -0400

    Added view link to status.html (for zwaveserver), and fixed show_node.html

--- examples/zwaveServer.py
+++ examples/zwaveServer.py
@@ -138,7 +138,7 @@ def list_nodes():
 @templated()
 @requires_network
 def show_node(id):
-    return dict(node=get_node(id))
+    return dict(node=get_node(int(id)))
 
     # TODO: command param, execute if set (and if method is put)
     # /nodes/<id>?command=on <-- validate command
